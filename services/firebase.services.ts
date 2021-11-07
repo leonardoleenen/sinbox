5ffc5fd1d7a6503ef393f3f1a1b4ab5082840155
Leonardo Leenen <leonardoleenen@gmail.com>
2021-11-07 06:50:19 -0300

    Fix Firebase VAR

--- services/firebase.services.ts
+++ services/firebase.services.ts
@@ -3,24 +3,14 @@ import { getFirestore, collection, getDocs } from 'firebase/firestore/lite'
 import '@firebase/firestore'
 
 class FirebaseManager {
-    /* private firebaseConfig = JSON.parse(
-        Buffer.from(process.env.FIREBASE_CONFIG as string, 'base64').toString()
-    ) */
-
     private firebaseApp: any
 
     constructor() {
-        /* if (!firebase.apps.length) {
-            this.firebaseApp = firebase.initializeApp(this.firebaseConfig)
-        } else {
-            this.firebaseApp = firebase.app() // if already initialized, use that one
-        } */
-
         if (process.env.FIREBASE_CONFIG) {
             this.firebaseApp = initializeApp(
                 JSON.parse(
                     Buffer.from(
-                        process.env.FIREBASE_CONFIG as string,
+                        process.env.FIREBASE as string,
                         'base64'
                     ).toString()
                 )
