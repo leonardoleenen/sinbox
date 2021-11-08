afb697a0eedcf95428553a1036298c4719634a94
Leonardo Leenen <leonardoleenen@gmail.com>
2021-11-08 16:55:02 -0300

    Improvements auth method

--- pages/index.tsx
+++ pages/index.tsx
@@ -2,14 +2,18 @@ import type { NextPage } from 'next'
 import Head from 'next/head'
 import Image from 'next/image'
 import { useEffect } from 'react'
-import { tokenDecode } from '../services/auth.service'
+import { setToken, tokenDecode } from '../services/auth.service'
 import styles from '../styles/Home.module.css'
+import { useRouter } from 'next/router'
 
 const Home: NextPage = () => {
-    useEffect( () => {
+    const router = useRouter()
+    const { token } = router.query
 
-        console.log(tokenDecode('eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOiIxMjM0NTY3ODkwIiwibmFtZSI6IkpvaG4gRG9lIiwiaWF0IjoxNTE2MjM5MDIyLCJpZCI6Imxlb25hcmRvbGVlbmVuQGdtYWlsLmNvbSIsInJvbGUiOiJFWFRFUk5BTCBVU0VSIn0.uXv8owTA-iKSsh7Bk7lOOb4lnbtQzX2KcYnEAeMvFm0'))
-    },[])
+    useEffect(() => {
+        setToken(token as string)
+        // tokenDecode(token).role=''
+    }, [token])
     return <div className={styles.container}>HOla Mundo!!!</div>
 }
 
--- services/auth.service.ts
+++ services/auth.service.ts
@@ -1,5 +1,9 @@
 import jwt from 'jsonwebtoken'
 
-export const tokenDecode = (token: string) : User => {
+export const tokenDecode = (token: string): User => {
     return jwt.decode(token) as User
+}
+
+export const setToken = (token: string) => {
+    localStorage.setItem('sinbox:token', token)
 }
