a705e39ee2b9a7d5909fd0c06dbaac01a1c59888
Maria Noel Leenen <leenen.maria@gmail.com>
2022-01-06 15:59:35 -0300

    note . tsx

---
+++ pages/note.tsx
@@ -0,0 +1,40 @@
+import React, { useEffect, useState } from 'react'
+import type { NextPage } from 'next'
+import { useRouter } from 'next/router'
+import { SignUpStore } from '../store/sigup.store'
+
+const Page: NextPage = () => {
+    const router = useRouter()
+    const state = SignUpStore.useState(s => s)
+    const [loading, setLoading] = useState(false)
+
+
+
+
+
+    return (
+        <div>
+            <section className="">
+                <div className=" flex justify-between">
+                    <div><img src='../logos/logoStaFe.png' /></div>
+                    <div className='w-2/4 '>
+                        <div className="form-control text-right mt-4 mr-8">
+
+                            <textarea className="textarea h-14 " placeholder="Texto de 3 lineas"></textarea>
+                            <textarea className="textarea h-4" placeholder="Aca va la fecha"></textarea>
+                       
+                        </div>
+                    </div>
+                </div>
+                <div className="form-control text-right mt-4 mr-8">
+
+                            <textarea className="textarea h-3/4 " placeholder="cuerpo"></textarea>
+                           
+                        </div>
+
+            </section>
+        </div>
+    )
+}
+
+export default Page
