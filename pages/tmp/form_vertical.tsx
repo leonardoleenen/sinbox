87891be1083a703874c55ab673ca48a641c5dbe4
Maria Noel Leenen <leenen.maria@gmail.com>
2022-07-17 12:02:12 -0300

    se sube el form_vertical

--- pages/tmp/form_vertical.tsx
+++ pages/tmp/form_vertical.tsx
@@ -1,9 +1,10 @@
 import React from 'react'
 import type { NextPage } from 'next'
 
-const Page: NextPage = () => {
+const FormVertical: NextPage = () => {
     return (
         <div>
+            {/*--HEADER--*/}
             <div className="border-slate-200 border-b-2 p-4 px-8 flex flex-row items-center">
                 <div className="basis-1/2">
                     <img
@@ -21,19 +22,36 @@ const Page: NextPage = () => {
                     </p>
                 </div>
                 <div className="basis-1/2 text-right">
-                    <h4 className="font-bold text-base ">
-                        Santa Fe,
-                        <br /> "Cuna de la Constitución Nacional"
+                    <h4 className="font-bold text-base">
+                        Santa Fe, "Cuna de la Constitución Nacional"
                     </h4>
                 </div>
             </div>
-            <div className=" p-4 px-8  text-xs  text-slate-600">
-                <div className=" text-right">
+            {/*--SUB HEADER--*/}
+            <div className="border-slate-200 border-b-2 p-4 px-8 border-dashed flex text-xs  text-slate-600">
+                <div className="basis-1/2">
+                    Campaña:
+                    <strong className="ml-2">
+                        Secretaria de Medios y Comunicacion Publica -
+                        Coronavirus-Nuevas Medidas de Aislamiento Octubre/2020
+                    </strong>
+                </div>
+                <div className="basis-1/2 text-right">
                     <div>
-                        Fecha:<strong className="ml-2">15/07/2022</strong>
+                        Orden de Publicidad N°:
+                        <strong className="ml-2">12/12/2020</strong>
+                    </div>
+                    <div>
+                        Expediente N°:
+                        <strong className="ml-2">4797/2022</strong>
+                    </div>
+                    <div>
+                        Fecha:<strong className="ml-2">12/12/2020</strong>
                     </div>
                 </div>
             </div>
+            {/*--BODY--*/}
+
             <div className=" p-4  text-xs px-8  mb-4 text-slate-600">
                 <h4 className="font-bold mb-4">
                     Señor Gerente o Administrador de:
@@ -53,41 +71,43 @@ const Page: NextPage = () => {
                     <div className="basis-1/2 mr-8">
                         <p className="mb-2">
                             Cuit:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">312312312312</strong>
                         </p>
                         <p className="mb-2">
                             Nombre del titular:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">Leonardo</strong>
                         </p>
                         <p className="mb-2">
                             Tipo moneda datos basicos:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">Pesos Argentinos</strong>
                         </p>
                         <p className="mb-2">
                             Telefono:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">3123123123</strong>
                         </p>
                         <p className="mb-2">
                             Sitio Web:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">www.</strong>
                         </p>
                     </div>
                     <div className="basis-1/2 pl-4  ">
                         <p className="mb-2">
                             Condicion Frente al Iva:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">
+                                Responsable Inscripto
+                            </strong>
                         </p>
                         <p className="mb-2">
                             Beneficiario:
-                            <strong className="ml-3">+541142728989</strong>
+                            <strong className="ml-3">Leonardo</strong>
                         </p>
                         <p className="mb-2">
                             Tipo de Medio:
-                            <strong className="ml-3">+541142728989</strong>
+                            <strong className="ml-3">web</strong>
                         </p>
                         <p className="mb-2">
                             Email:
-                            <strong className="ml-3">+541142728989</strong>
+                            <strong className="ml-3">leenene@gmail</strong>
                         </p>
                     </div>
                 </div>
@@ -101,41 +121,41 @@ const Page: NextPage = () => {
                     <div className="basis-1/2 mr-8">
                         <p className="mb-2">
                             Calle:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">Chocano</strong>
                         </p>
                         <p className="mb-2">
                             Piso:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">3</strong>
                         </p>
                         <p className="mb-2">
                             Pais:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">Argentina</strong>
                         </p>
                         <p className="mb-2">
                             Localidad:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">Lomas de zamora</strong>
                         </p>
                         <p className="mb-2">
                             Observaciones:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">chocano</strong>
                         </p>
                     </div>
                     <div className="basis-1/2 pl-4  ">
                         <p className="mb-2">
                             Numero:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">195</strong>
                         </p>
                         <p className="mb-2">
                             Departamento:
-                            <strong className="ml-3">+541142728989</strong>
+                            <strong className="ml-3">3</strong>
                         </p>
                         <p className="mb-2">
                             Provincia:
-                            <strong className="ml-3">+541142728989</strong>
+                            <strong className="ml-3">Buenos Aires</strong>
                         </p>
                         <p className="mb-2">
                             Municipio:
-                            <strong className="ml-3">+541142728989</strong>
+                            <strong className="ml-3">lomas de zamora</strong>
                         </p>
                     </div>
                 </div>
@@ -148,29 +168,29 @@ const Page: NextPage = () => {
                     <div className="basis-1/2 mr-8">
                         <p className="mb-2">
                             Cuit:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">3123123123123</strong>
                         </p>
                         <p className="mb-2">
                             Nombre del Responsable:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">Leonardo</strong>
                         </p>
                         <p className="mb-2">
                             Telefono:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">3213123123123</strong>
                         </p>
                     </div>
                     <div className="basis-1/2 pl-4  ">
                         <p className="mb-2">
                             Nombre del titular:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">Leonardo</strong>
                         </p>
                         <p className="mb-2">
                             Email:
-                            <strong className="ml-3">+541142728989</strong>
+                            <strong className="ml-3">leenene@gmail</strong>
                         </p>
                         <p className="mb-2">
                             Telefono Alternativo:
-                            <strong className="ml-3">+541142728989</strong>
+                            <strong className="ml-3">212212</strong>
                         </p>
                     </div>
                 </div>
@@ -183,25 +203,25 @@ const Page: NextPage = () => {
                     <div className="basis-1/2 mr-8">
                         <p className="mb-2">
                             Nombre:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">312312312</strong>
                         </p>
                         <p className="mb-2">
                             Telefono:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">12312312</strong>
                         </p>
                         <p className="mb-2">
                             Email:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">leeenen@gmail.com</strong>
                         </p>
                     </div>
                     <div className="basis-1/2 pl-4  ">
                         <p className="mb-2">
                             Apellido:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">Leenen</strong>
                         </p>
                         <p className="mb-2">
                             Telefono Alternativo:
-                            <strong className="ml-3">+541142728989</strong>
+                            <strong className="ml-3">312312312</strong>
                         </p>
                     </div>
                 </div>
@@ -214,25 +234,25 @@ const Page: NextPage = () => {
                     <div className="basis-1/2 mr-8">
                         <p className="mb-2">
                             Nombre:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">Leonardo</strong>
                         </p>
                         <p className="mb-2">
                             Telefono:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">3242423</strong>
                         </p>
                         <p className="mb-2">
                             Email:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">leenene@gmail.com</strong>
                         </p>
                     </div>
                     <div className="basis-1/2 pl-4  ">
                         <p className="mb-2">
                             Apellido:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">Leenen</strong>
                         </p>
                         <p className="mb-2">
                             Telefono Alternativo:
-                            <strong className="ml-3">+541142728989</strong>
+                            <strong className="ml-3">1134566665</strong>
                         </p>
                     </div>
                 </div>
@@ -245,21 +265,21 @@ const Page: NextPage = () => {
                     <div className="basis-1/2 mr-8">
                         <p className="mb-2">
                             Norma / Resolucion:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">312312</strong>
                         </p>
                         <p className="mb-2">
                             Fecha de Vigencia:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">15/07/2022</strong>
                         </p>
                     </div>
                     <div className="basis-1/2 pl-4  ">
                         <p className="mb-2">
                             Año:
-                            <strong className="ml-3"></strong>
+                            <strong className="ml-3">2022</strong>
                         </p>
                         <p className="mb-2">
                             Numero de Norma:
-                            <strong className="ml-3">+541142728989</strong>
+                            <strong className="ml-3">21321</strong>
                         </p>
                     </div>
                 </div>
@@ -272,19 +292,22 @@ const Page: NextPage = () => {
                     <div className="basis-1/2 mr-8">
                         <p className="mb-2">
                             Importe:
-                            <strong className="ml-3">Radio mitre</strong>
+                            <strong className="ml-3">$8.999,99</strong>
                         </p>
                     </div>
                     <div className="basis-1/2 pl-4  ">
                         <p className="mb-2">
                             Nombre:
-                            <strong className="ml-3">12312312321</strong>
+                            <strong className="ml-3">Por la tarde</strong>
                         </p>
                     </div>
                 </div>
+            </div>
+            <div className="border-slate-200 border-t-2 p-4 px-8 flex flex-row  text-center items-center mt-8 bottom-0">
+                ORIGINAL
             </div>
         </div>
     )
 }
 
-export default Page
+export default FormVertical
